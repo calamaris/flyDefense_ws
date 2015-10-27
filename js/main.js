eea80e7849935d596d5e90295452d243bd1dee4b
Oscar Crespo <crespo.krw@gmail.com>
2015-10-27 17:08:52 -0600

    limpio

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
 
 $(document).ready(function(){
-<<<<<<< HEAD
-	if ($("body").width()>=1023); {
-		var s = skrollr.init();
-		console.log("se cargo skrollr");
-=======
 	if ($("body").width()>=1023) {
 		var s = skrollr.init({
 				    forceHeight: false
 				});
 		console.log("se cargo skrollr")
->>>>>>> origin/master
 	};
-
 });
